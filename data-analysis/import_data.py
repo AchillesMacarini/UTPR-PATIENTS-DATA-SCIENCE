a5cc2280f9425d5265b2c55e8e47ed9a9c74cc27
AchillesMacarini <capivaradoacre@gmail.com>
2024-11-02 13:05:15 -0300

    feat: add classes and features

--- data-analysis/import_data.py
+++ data-analysis/import_data.py
@@ -1,7 +1,11 @@
 import os
 import pandas as pd
 
-script_dir = os.path.dirname(__file__)  # Diretório do script
+script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, '..', 'data-set', 'patients-data.csv')
 
 data = pd.read_csv(file_path)
+classes = data['PatientID']
+features = data.drop(columns=['PatientID'])
+
+print(data.head)
