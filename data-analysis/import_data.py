80505311e6c4efb0082efa8c3d054ba3529a3d73
Eduardo Andreoli <53129882+C3Andreoli@users.noreply.github.com>
2024-11-02 14:00:55 -0300

    Categorização e discretização dos dados qualitativos
    Para facilitar a análise, tornamos os dados qualitativos em quantitativos

--- data-analysis/import_data.py
+++ data-analysis/import_data.py
@@ -4,6 +4,14 @@ import pandas as pd
 script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, '..', 'data-set', 'patients-data.csv')
 
-data = pd.read_csv(file_path)
+raw_data = pd.read_csv(file_path)
+data = raw_data
+for arquivo in os.listdir(os.path.join(script_dir,'..','data-set')):
+    if arquivo.endswith('_discretizacao.csv'):
+        df = pd.read_csv(os.path.join(script_dir, '..', 'data-set', arquivo))
+        mapping = df.set_index(df.columns[0])['ID'].to_dict()
+        data[df.columns[0]] = data[df.columns[0]].map(mapping)
+
 classes = data['PatientID']
 features = data.drop(columns=['PatientID'])
+
