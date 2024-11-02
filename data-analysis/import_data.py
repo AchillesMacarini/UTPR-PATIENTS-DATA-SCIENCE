4e12d7d4728a3f578f5fa9cc809bb2439fe7614e
AchillesMacarini <capivaradoacre@gmail.com>
2024-11-02 13:06:05 -0300

    fix: removing print from import

--- data-analysis/import_data.py
+++ data-analysis/import_data.py
@@ -7,5 +7,3 @@ file_path = os.path.join(script_dir, '..', 'data-set', 'patients-data.csv')
 data = pd.read_csv(file_path)
 classes = data['PatientID']
 features = data.drop(columns=['PatientID'])
-
-print(data.head)
